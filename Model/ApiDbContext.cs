d50aa10399e91efee8ea42630176a9c366fe080a
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-09-29 14:28:22 -0300

    Created ApiDbContext and installed EF Core

---
+++ Model/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaAPICore.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Model/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaAPICore.Model
+{
+    public class DbContext
+    {
+        private DbContextOptions options;
+
+        public DbContext(DbContextOptions options)
+        {
+            this.options = options;
+        }
+    }
+}
