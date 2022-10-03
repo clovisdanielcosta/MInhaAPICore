a6b97c8e17f5653c6726ca66db06c9de8289db78
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2022-10-03 15:23:10 -0300

    Updated FornecedorViewModel and ProdutoViewModel

--- ViewModels/FornecedorViewModel.cs
+++ ViewModels/FornecedorViewModel.cs
@@ -16,12 +16,10 @@ namespace MinhaAPICore.ViewModels
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
 
-        [DisplayName("Tipo")]
         public int TipoFornecedor { get; set; }
 
         public EnderecoViewModel Endereco { get; set; }
 
-        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
         public IEnumerable<ProdutoViewModel> Produtos { get; set; }
